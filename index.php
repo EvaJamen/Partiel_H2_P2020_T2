731d13763fc46c70e30dfbd06f0fc2bdbd3335f5
EvaJamen <jamen.eva@gmail.fr>
2017-03-23 02:36:29 +0100

    Create template

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+	//CONNECT TO MY BDD
+	include 'includes/config.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Home</title>
+		<link rel="stylesheet" href="src/css/style.css" />
+	</head>
+	<body>
+	</body>
+</html>
